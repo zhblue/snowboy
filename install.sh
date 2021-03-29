f8c32c57c4c0916b64a12666ec88e963ad9efdf5
zhblue <newsclan@gmail.com>
2021-03-29 18:48:21 +0800

    Servo of PCA9685

---
+++ install.sh
@@ -0,0 +1,6 @@
+ sudo apt-get install python-pip
+ sudo pip install adafruit-pca9685
+ sudo apt-get install python3-pip
+ sudo pip3 install adafruit-pca9685
+sudo apt-get install python3-scipy python3-pyaudio
+sudo apt-get install python-scipy python-pyaudio
