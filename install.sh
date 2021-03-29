aa8fec5a979b5450c65a21fe621db3a6550dc584
张浩斌 <newsclan@gmail.com>
2021-03-29 19:53:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get install python-pip python-scipy python-pyaudio
 sudo pip install adafruit-pca9685
 sudo apt-get install python3-pip python3-scipy python3-pyaudio
 sudo pip3 install adafruit-pca9685
-sudo apt-get install i2c-tools wiringpi swig sox
+sudo apt-get install i2c-tools wiringpi swig sox libatlas-base-dev portaudio19-dev
 cd /snowboy/swig/Python3
 make
 
