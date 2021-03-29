fb7de1fe5589b0787c297c7e05419c8a21ecb536
张浩斌 <newsclan@gmail.com>
2021-03-29 19:49:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
- sudo apt-get install python-pip
- sudo pip install adafruit-pca9685
- sudo apt-get install python3-pip
- sudo pip3 install adafruit-pca9685
-sudo apt-get install python3-scipy python3-pyaudio
-sudo apt-get install python-scipy python-pyaudio
+#!/bin/bash
+sudo apt-get install python-pip python-scipy python-pyaudio 
+sudo pip install adafruit-pca9685
+sudo apt-get install python3-pip python3-scipy python3-pyaudio
+sudo pip3 install adafruit-pca9685
+sudo apt-get install i2c-tools wiringpi swig sox
+
