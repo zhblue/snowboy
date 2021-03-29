0b55ebd5679b361610503bab516fe55fc8486982
张浩斌 <newsclan@gmail.com>
2021-03-29 20:02:00 +0800

    Update trash.py

--- examples/Python3/trash.py
+++ examples/Python3/trash.py
@@ -1,7 +1,43 @@
+from __future__ import division
 import snowboydecoder
 import sys
 import os
 import signal
+import time
+
+# Import the PCA9685 module.
+import Adafruit_PCA9685
+
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
+# Alternatively specify a different address and/or bus:
+#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+
+# Configure min and max servo pulse lengths
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 600  # Max pulse length out of 4096
+
+# Helper function to make setting a servo pulse width simpler.
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(60)
+
+print('Moving servo on channel 0, press Ctrl-C to quit...')
 
 # Demo code for listening to two hotwords at the same time
 
@@ -10,11 +46,17 @@ def openKitchen():
     print("Kitchen")
     snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
     os.system("lcd 4 5 Kitchen")
+    pwm.set_pwm(0, 0, 300)
+    time.sleep(2)
+    pwm.set_pwm(0, 0, servo_max)
 
 def openOther():
     print("Other")
     snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
     os.system("lcd 4 5 Other")
+    pwm.set_pwm(1, 0, 302)
+    time.sleep(2)
+    pwm.set_pwm(1, 0, servo_max)
 
 def signal_handler(signal, frame):
     global interrupted
